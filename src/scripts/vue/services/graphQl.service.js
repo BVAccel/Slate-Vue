af0939933eb1d3111c63ca86b7790df22e98a299
Jon Iler <jon.iler@bvaccel.com>
2019-08-26 13:28:02 -0700

    began writing new queries and starting on query service folder structure

--- src/scripts/vue/services/graphQl.service.js
+++ src/scripts/vue/services/graphQl.service.js
@@ -1,29 +1,25 @@
-import { GraphQLClient } from "graphql-request";
-import { QueryService} from './query.service';
+import { GraphQLClient } from 'graphql-request';
+import { QueryService } from './query.service';
 const queryService = new QueryService();
-const TOKEN ='';
-const STORE ='';
-const client = new GraphQLClient(STORE, { headers: {  "X-Shopify-Storefront-Access-Token": TOKEN} })
-export class GraphQlService{
+const TOKEN = '01fd7447bdcd2b706797f474fbc12ccf';
+const STORE = '';
+const client = new GraphQLClient(STORE, { headers: { 'X-Shopify-Storefront-Access-Token': TOKEN } });
+export class GraphQlService {
+  constructor() {}
 
-    constructor(){ }
+  /**
+   * FIELDS
+   */
 
-    /**
-     * FIELDS
-     */
-
-    getCollectionByHandle(handleId){
-        let promise = new Promise((resolve,reject)=>{
-          // Get Graphql query
-          let productQuery = queryService.getCollectionQuery(handleId);
-          // Fetch query data
-          client.request(productQuery).then(data => {
-              resolve(data)
-          })
-        });
-        return promise;
-    }
-
-
-
+  getCollectionByHandle(handleId, productQuantity, variantQuantity) {
+    let promise = new Promise((resolve, reject) => {
+      // Get Graphql query
+      let productQuery = queryService.getCollectionQuery(handleId, productQuantity, variantQuantity);
+      // Fetch query data
+      client.request(productQuery).then((data) => {
+        resolve(data);
+      });
+    });
+    return promise;
+  }
 }
--- src/scripts/vue/services/query.service.js
+++ src/scripts/vue/services/query.service.js
@@ -1,43 +1,41 @@
-export class QueryService{
+export class QueryService {
+  getCollectionQuery(handleId, prevQuantity, productQuantity, variantQuantity) {
+    let query = `
+      {
+        collectionByHandle(handle:"${handleId}") {
+          id
+          products(first:${productQuantity} offset:${prevQuantity}) {
+            edges {
+              node {
+                id
 
-    getCollectionQuery(handleId){
-        let query = `
-        {
-            collectionByHandle(handle:"${handleId}") {
-              id
-              products(first:10) {
-                edges {
-                  node {
-                    id
-
-                    images(first:2)  {
-                      edges {
-                        node {
-                          id
-                          originalSrc
-                        }
-                      }
+                images(first:2)  {
+                  edges {
+                    node {
+                      id
+                      originalSrc
                     }
-                    title
-                    variants(first:10) {
-                      edges {
-                        node {
-                          id
-                          title
-                          price
-                          image{
-                            id
-                            originalSrc
-                          }
-                        }
+                  }
+                }
+                title
+                variants(first:${variantQuantity}) {
+                  edges {
+                    node {
+                      id
+                      title
+                      price
+                      image{
+                        id
+                        originalSrc
                       }
                     }
                   }
                 }
               }
             }
-          }`;
-        return query
+          }
+        }
       }
-
+    `
+  }
 }
