e6cedaa2ea531d2f6cc8395d1db68dfd5f19ac7e
Jon Iler <jon.iler@bvaccel.com>
2019-08-27 13:00:31 -0700

    renamed gql client service files using kebab case

--- src/scripts/vue/core/services.js
+++ src/scripts/vue/core/services.js
@@ -8,8 +8,8 @@ import { EventBus, CloseCartModal, OpenCartModal, CartUpdated } from './services
 import { CartService } from './services/cart.service'
 
 // GraphQL Services
-import { CollectionClient } from './services/graphql/collectionClient.service'
-import { SearchClient } from './services/graphql/searchClient.service'
+import { CollectionClient } from './services/graphql/collection-client.service'
+import { SearchClient } from './services/graphql/search-client.service'
 import { CollectionQueries } from './services/graphql/queries/collection'
 import { SearchQueries } from './services/graphql/queries/search'
 
