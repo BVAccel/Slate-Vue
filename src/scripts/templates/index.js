3c7ceb9d41a11b537347484db37e211af86bf6ca
Vladimir Skok <skok@vova.io>
2019-08-27 14:33:58 -0700

    style slate and vue index.js templates

--- src/scripts/templates/index.js
+++ src/scripts/templates/index.js
@@ -1,54 +1,53 @@
 import 'styles/templates/index/index.scss';
 import IndexTemplate from '../vue/templates/index/index.vue';
 import ButtonPrimary from '../vue/shared/components/buttons/primary-button.vue';
-import SectionTitle  from '../vue/shared/components/titles/section-title.vue';
+import SectionTitle from '../vue/shared/components/titles/section-title.vue';
 import ValuePropCard from '../vue/shared/components/cards/value-prop-card.vue';
-import HeroSlider  from '../vue/shared/components/sliders/hero-slider.vue';
+import HeroSlider from '../vue/shared/components/sliders/hero-slider.vue';
 import priceFilter from '../vue/shared/filters/price'
 
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => { });
 
 window.addEventListener('load', () => {
-    init();
+  init();
 });
 
 const initVue = () => {
-
-    Vue.filter('uppercase', function (value) {
-        if (!value) return ''
-        value = value.toString().toUpperCase()
-        return value
-      })
+  Vue.filter('uppercase', function (value) {
+    if (!value) return ''
+    value = value.toString().toUpperCase()
+    return value
+  })
 
   Vue.filter('price', priceFilter);
 
-    /**
-     * INIT VUE APP
-     */
-    new Vue({
-        delimiters: ['${', '}'],
-        el: '#app-index',
-        data: {
-        },
-        components:{
-            // Wrapper Component
-            IndexTemplate,
-            ButtonPrimary,
-            SectionTitle,
-            ValuePropCard,
-            HeroSlider
-        },
-        methods: {
+  /**
+   * INIT VUE APP
+   */
+  new Vue({
+    delimiters: ['${', '}'],
+    el: '#app-index',
+    data: {
+    },
+    components: {
+      // Wrapper Component
+      IndexTemplate,
+      ButtonPrimary,
+      SectionTitle,
+      ValuePropCard,
+      HeroSlider
+    },
+    methods: {
 
-        }
-    });
+    }
+  });
 }
 
 
 // Init all functions here.
 export const init = () => {
-    initVue();
+  initVue();
 };
 
 
