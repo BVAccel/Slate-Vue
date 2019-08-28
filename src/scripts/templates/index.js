f031d36404002f29924ecc871aefb75548a5fb92
Vladimir Skok <iamskok@users.noreply.github.com>
2019-08-28 10:53:16 -0700

    Currency filter (#4)
    * add currency filter
    
    * style slate and vue index.js templates
    
    * move uppercase filter in ./shared/filters

--- src/scripts/templates/index.js
+++ src/scripts/templates/index.js
@@ -1,52 +1,39 @@
 import 'styles/templates/index/index.scss';
 import IndexTemplate from '../vue/templates/index/index.vue';
 import ButtonPrimary from '../vue/shared/components/buttons/primary-button.vue';
-import SectionTitle  from '../vue/shared/components/titles/section-title.vue';
+import SectionTitle from '../vue/shared/components/titles/section-title.vue';
 import ValuePropCard from '../vue/shared/components/cards/value-prop-card.vue';
-import HeroSlider  from '../vue/shared/components/sliders/hero-slider.vue';
+import HeroSlider from '../vue/shared/components/sliders/hero-slider.vue';
+import priceFilter from '../vue/shared/filters/price'
+import uppercaseFilter from '../vue/shared/filters/uppercase'
 
 document.addEventListener('DOMContentLoaded', () => {});
 
-window.addEventListener('load', () => {
-    init();
-});
+window.addEventListener('load', () => init());
 
 const initVue = () => {
+  Vue.filter('uppercase', uppercaseFilter);
+  Vue.filter('price', priceFilter);
 
-    Vue.filter('uppercase', function (value) {
-        if (!value) return ''
-        value = value.toString().toUpperCase()
-        return value
-      })
-
-    /**
-     * INIT VUE APP
-     */
-    new Vue({
-        delimiters: ['${', '}'],
-        el: '#app-index',
-        data: {
-        },
-        components:{
-            // Wrapper Component
-            IndexTemplate,
-            ButtonPrimary,
-            SectionTitle,
-            ValuePropCard,
-            HeroSlider
-        },
-        methods: {
-
-        }
-    });
+  /**
+   * INIT VUE APP
+   */
+  new Vue({
+    delimiters: ['${', '}'],
+    el: '#app-index',
+    data: {
+    },
+    components: {
+      // Wrapper Component
+      IndexTemplate,
+      ButtonPrimary,
+      SectionTitle,
+      ValuePropCard,
+      HeroSlider
+    }
+  });
 }
 
 
 // Init all functions here.
-export const init = () => {
-    initVue();
-};
-
-
-
-
+export const init = () => initVue();
