df5d39e245eae43a2caeac10ff1ba773256146d8
dlerm <younglerman@gmail.com>
2019-09-06 18:33:19 -0700

    Section data API service setup

---
+++ src/scripts/vue/core/vue-section-boilerplate.vue
@@ -0,0 +1,45 @@
+// Template
+<template>
+  <!-- Build section using props: settings & blocks -->
+  <section v-if="settings.enabled">
+    <ul v-if="blocks">
+      <li v-for="key in block_order" v-if="blocks[key].settings.enabled">
+        <img :src="blocks[key].settings.image" />
+        <p v-if="blocks[key].settings.title" v-text="blocks[key].settings.title"></p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+// JavaScript
+<script>
+export default {
+  name: 'VueSectionBoilerplate',
+  props: {
+    theme_settings: {
+      type: Object,
+      required: false,
+    },
+    settings: {
+      type: Object,
+      required: false,
+    },
+    blocks: {
+      type: Object,
+      required: false,
+    },
+    block_order: {
+      type: Array,
+      required: false,
+    },
+  },
+  created: function() {
+    // console.log(this.theme_settings);
+    // console.log(this.settings);
+    console.log(this.blocks);
+  },
+};
+</script>
+
+// SCSS
+<style lang="scss" scoped></style>
--- src/scripts/vue/templates/index/index.vue
+++ src/scripts/vue/templates/index/index.vue
@@ -1,9 +1,11 @@
-
 // template
-<template >
+<template>
   <div>
     <!-- Section Outlet-->
-    <slot></slot>
+    <!-- <slot></slot> -->
+    <slot name="content_for_index"></slot>
+
+    <!-- Buttons -->
     <div style="padding: 10px;">
       <primary-button text="OPEN CART" @buttonClick="onOpenCartClick()"></primary-button>
       <primary-button text="CLOSE CART" @buttonClick="onCloseCartClick()"></primary-button>
@@ -11,21 +13,25 @@
       <primary-button text="CART DATA" @buttonClick="onGetCartClick()"></primary-button>
     </div>
 
+    <!-- Vue Section Example -->
+    <!-- <slot name="vue-section-boilerplate"></slot> -->
+    <vue-section-boilerplate :settings="this.section.settings" :blocks="this.section.blocks" :block_order="this.section.block_order"> </vue-section-boilerplate>
+
     <!-- Pagination Example -->
     <div class="center">
       <div class="flex-wrap">
         <div v-for="item in items" :key="item.id" class="item-card">
           <p>
             <b>Name:</b>
-            {{item.employee_name}}
+            {{ item.employee_name }}
           </p>
           <p>
             <b>ID:</b>
-            {{item.id}}
+            {{ item.id }}
           </p>
           <p>
             <b>Price:</b>
-            {{item.employee_salary | price }}
+            {{ item.employee_salary | price }}
           </p>
           <primary-button class="item-card-button" text="EDIT"></primary-button>
         </div>
@@ -33,12 +39,7 @@
     </div>
 
     <div class="center">
-      <general-pagination
-        :numberOfPaginationButtons="numberOfPaginationButtons"
-        :numberOfItemsPerPage="numberOfItemsPerPage"
-        :totalNumberOfItems="dummyData.length"
-        @pagination="onPagination"
-      ></general-pagination>
+      <general-pagination :numberOfPaginationButtons="numberOfPaginationButtons" :numberOfItemsPerPage="numberOfItemsPerPage" :totalNumberOfItems="dummyData.length" @pagination="onPagination"></general-pagination>
     </div>
   </div>
 </template>
@@ -77,82 +78,89 @@ li {
 
 // JavaScript
 <script>
-    /******************
-     * INDEX TEMPLATE *
-     *****************/
-    import { PrimaryButton, IconButton, GeneralPagination } from '../../shared/components';
-    import { ShopifyService, EventBus, OpenCartModal, CloseCartModal, CartService } from '../../core/services';
+/******************
+ * INDEX TEMPLATE *
+ *****************/
+import { PrimaryButton, IconButton, GeneralPagination } from '../../shared/components';
+// import { VueSectionBoilerplate } from '../../core/components';
+import { ShopifyService, EventBus, OpenCartModal, CloseCartModal, CartService } from '../../core/services';
 
-    const cartService = new CartService();
-    const shopifyService = new ShopifyService();
-    const ComponentName = 'Index';
+const cartService = new CartService();
+const shopifyService = new ShopifyService();
+const ComponentName = 'Index';
 
+export default {
+  // Component Bindings
+  name: ComponentName,
+  props: {
+    exampleProp: String,
+  },
+  // Component Data
+  data: function() {
+    return {
+      exampleArr: [],
+      dummyData: [],
+      // Pagination
+      numberOfPaginationButtons: Number,
+      activePage: Number,
+      pageNumberArray: [],
+      // Pagination Data
+      numberOfItemsPerPage: Number,
+      items: [],
+      section: {},
+    };
+  },
+  components: {
+    PrimaryButton,
+    IconButton,
+    GeneralPagination,
+    'vue-section-boilerplate': () => import('../../core/vue-section-boilerplate.vue'),
+  },
+  methods: {
+    onPagination(dataSection) {
+      console.log('pagination start/end', dataSection);
+      this.setPageData(dataSection);
+    },
+    onOpenCartClick() {
+      EventBus.$emit(OpenCartModal);
+    },
+    onCloseCartClick() {
+      EventBus.$emit(CloseCartModal);
+    },
+    onButtonClick() {
+      const ExampleItem = 29343910395978;
+      cartService.addItem(1, ExampleItem);
+    },
+    onGetCartClick() {
+      cartService.getCartData().then((resp) => {
+        console.log('cart items', resp);
+      });
+    },
+    setPageData(dataSection) {
+      if (dataSection) {
+        this.items = this.dummyData.slice(dataSection.start, dataSection.end);
+      } else {
+        // if not set. set to 20 items
+        this.items = this.dummyData.slice(0, this.numberOfItemsPerPage);
+      }
+    },
+    initPagination() {
+      this.numberOfPaginationButtons = 5;
+      this.numberOfItemsPerPage = 20;
+      let dataSection = { start: 0, end: 20 };
+      this.setPageData(dataSection);
+    },
+  },
+  created: function() {
+    // Runs on Init
+    this.dummyData = shopifyService.getDummyData();
+    //this needs to come after ajax promise
+    this.initPagination();
 
-  export default {
-    // Component Bindings
-    name: ComponentName,
-    props: {
-      exampleProp: String,
-    },
-    // Component Data
-    data: function() {
-      return {
-        exampleArr: [],
-        dummyData: [],
-        // Pagination
-        numberOfPaginationButtons: Number,
-        activePage: Number,
-        pageNumberArray: [],
-        // Pagination Data
-        numberOfItemsPerPage: Number,
-        items: [],
-      };
-    },
-    components: {
-      PrimaryButton,
-      IconButton,
-      GeneralPagination,
-    },
-    methods: {
-      onPagination(dataSection) {
-        console.log('pagination start/end', dataSection);
-        this.setPageData(dataSection);
-      },
-      onOpenCartClick() {
-        EventBus.$emit(OpenCartModal);
-      },
-      onCloseCartClick() {
-        EventBus.$emit(CloseCartModal);
-      },
-      onButtonClick() {
-        const ExampleItem = 29343910395978;
-        cartService.addItem(1, ExampleItem);
-      },
-      onGetCartClick() {
-        cartService.getCartData().then((resp) => {
-          console.log('cart items', resp);
-        });
-      },
-      setPageData(dataSection) {
-        if (dataSection) {
-          this.items = this.dummyData.slice(dataSection.start, dataSection.end);
-        } else {
-          // if not set. set to 20 items
-          this.items = this.dummyData.slice(0, this.numberOfItemsPerPage);
-        }
-      },
-      initPagination() {
-        this.numberOfPaginationButtons = 5;
-        this.numberOfItemsPerPage = 20;
-        let dataSection = { start: 0, end: 20 };
-        this.setPageData(dataSection);
-      },
-    },
-    created: function() {
-      // Runs on Init
-      this.dummyData = shopifyService.getDummyData();
-      //this needs to come after ajax promise
-      this.initPagination();
-    }
-  };
+    shopifyService.getSectionData('vue-section-boilerplate').then((res) => {
+      this.section = res;
+      console.log(this.section);
+    });
+  },
+};
 </script>
