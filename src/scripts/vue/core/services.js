e586a765ba66d4b5f59f92e3a478549fdff9b5d0
dlerm <younglerman@gmail.com>
2019-09-17 10:14:46 -0700

    Setup JSONTemplateService

--- src/scripts/vue/core/services.js
+++ src/scripts/vue/core/services.js
@@ -1,27 +1,19 @@
 // Shopify Services
-import { ShopifyService } from './services/shopify.service'
+import { ShopifyService } from './services/shopify.service';
 
 // Event Services
-import { EventBus, CloseCartModal, OpenCartModal, CartUpdated } from './services/event.service'
+import { EventBus, CloseCartModal, OpenCartModal, CartUpdated } from './services/event.service';
 
-// Cart Services 
-import { CartService } from './services/cart.service'
+// Cart Services
+import { CartService } from './services/cart.service';
+
+// Cart Services
+import { JSONTemplateService } from './services/json-template.service';
 
 // GraphQL Services
-import { CollectionClient } from './services/graphql/collection-client.service'
-import { SearchClient } from './services/graphql/search-client.service'
-import { CollectionQueries } from './services/graphql/queries/collection'
-import { SearchQueries } from './services/graphql/queries/search'
+import { CollectionClient } from './services/graphql/collection-client.service';
+import { SearchClient } from './services/graphql/search-client.service';
+import { CollectionQueries } from './services/graphql/queries/collection';
+import { SearchQueries } from './services/graphql/queries/search';
 
-export { 
-  ShopifyService, 
-  EventBus, 
-  CloseCartModal, 
-  OpenCartModal, 
-  CartUpdated, 
-  CartService, 
-  CollectionClient, 
-  SearchClient, 
-  CollectionQueries, 
-  SearchQueries 
-};
+export { ShopifyService, EventBus, CloseCartModal, OpenCartModal, CartUpdated, CartService, JSONTemplateService, CollectionClient, SearchClient, CollectionQueries, SearchQueries };
